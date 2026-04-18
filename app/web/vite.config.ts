3b29047e1c0226994e7e452c2358f67594fde124
Tarang74 <31427635+Tarang74@users.noreply.github.com>
2026-04-19 03:58:02 +1000

    Remove path prefix

--- app/web/vite.config.ts
+++ app/web/vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
 
   return {
-    base: "/fluid-sim/",
+    base: "/",
     plugins: [react()],
     worker: {
       format: "es",
